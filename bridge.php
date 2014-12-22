fbc505fa20a80ce383201ef79877b47fe674b2e3
ted <16161576@qq.com>
2014-12-22 17:55:32 +0800

    Revert "Revert "2014-12-12""
    This reverts commit fb9008057801d972b290d480cf15a6a14784ec1f.

---
+++ bridge.php
@@ -0,0 +1,10 @@
+<?php 
+include_once '/command/mysql.class.php';
+include_once '/command/config.php';
+
+
+
+$db = new mysql($mydbhost, $mydbuser, $mydbpw, $mydbname, '', $mydbcharset);
+
+
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once 'bridge.php';
+
+
+echo "hello word";
+?>
