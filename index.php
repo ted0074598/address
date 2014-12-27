bc15cd15ee8e6c9a32a2d9fbcd504b1049ca89f2
ted <16161576@qq.com>
2014-12-27 17:47:34 +0800

    2014-12-27
    开始前端设计

--- index.php
+++ index.php
@@ -1,29 +1,58 @@
-<?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/css.css" />
 
-//include_once 'bridge.php';
+<title>县局</title>
+</head>
+
+<body>
 
 
-echo "hello word";
+			<div class='head' id ='head'>
+				
+				<div class='head_top'>
+				</div>
+					
+					<div class='head_body'>
+					         
+					         <div class='title'>一二三四五
+					         </div>
+					       
+					</div>
+				
+				<div class='head_foot'>
+				</div>
+			
+						
+			</div>
+			<div id ='dbody'>
+			           
+			
+			</div>
+			<div id ='foot'>
+			
+			
+			</div>
+			
 
 
-$link = mysqli_connect("localhost","root","","address") or die("Error " . mysqli_error($link)); 
 
-//consultation: 
 
-$query = "SELECT * FROM `p_information`" or die("Error in the consult.." . mysqli_error($link)); 
 
-//execute the query. 
-$link->query("SET character_set_client='utf8'");
-$link->query("SET character_set_connection='utf8'");
-$link->query("SET character_set_results='utf8'");
 
-		
-$result = $link->query($query); 
 
-//display information: 
 
-while($row = mysqli_fetch_array($result)) { 
-echo $row["p_name"] . "<br>"; 
-}
 
-?>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
